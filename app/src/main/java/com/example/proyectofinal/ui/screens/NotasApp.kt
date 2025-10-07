cf751ba88feaa0c9df70e159f7ac2be32892a0d6
Alejandro Pérez <133078626+Alejandroperezitsur@users.noreply.github.com>
2025-10-07 00:07:50 -0600

    Implementacion de la navegacion

--- app/src/main/java/com/example/proyectofinal/ui/screens/NotasApp.kt
+++ app/src/main/java/com/example/proyectofinal/ui/screens/NotasApp.kt
@@ -8,14 +8,17 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.proyectofinal.navigation.NotasNavGraph
 import com.example.proyectofinal.ui.theme.ProyectoFinalTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotasApp() {
     var searchExpanded by remember { mutableStateOf(false) }
+    
+    val navController = rememberNavController()
     
     Scaffold(
         topBar = {
@@ -35,7 +38,7 @@ fun NotasApp() {
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = { /* TODO: Navegar a crear nota */ }
+                onClick = { navController.navigate("crear_nota") }
             ) {
                 Icon(Icons.Default.Add, contentDescription = "Nueva Nota")
             }
@@ -46,8 +49,7 @@ fun NotasApp() {
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            // TODO: Implementar NavigationHost aquí
-            Text("Contenido Principal")
+            NotasNavGraph(navController = navController)
         }
     }
 }
